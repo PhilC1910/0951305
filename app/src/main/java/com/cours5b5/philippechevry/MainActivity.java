e09aaccb4f50cf5338c31f246b2f01e04267e84a
philippe chevry <phil_chevry@hotmail.com>
2018-08-27 12:53:47 -0400

    atelier 2 langue et orientation dans le log

--- app/src/main/java/com/cours5b5/philippechevry/MainActivity.java
+++ app/src/main/java/com/cours5b5/philippechevry/MainActivity.java
@@ -9,7 +9,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("MonEtiquette", "Bonjour");
+        Log.d("Anglais", this.getResources().getString(R.string.langue));
+        Log.d("oriantation", this.getResources().getString(R.string.orientation));
         setContentView(R.layout.activity_main);
     }
 }
