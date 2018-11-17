30732fd81be731eeebab7412f0f0a239c84d689c
philippe chevry <phil_chevry@hotmail.com>
2018-11-16 21:54:38 -0500

    j'ai complete les méthode du proxyLyste. Il me reste à faire  dans MPartieReseau fournirActionPlacerJeton, dans APartieReseau le onPause et onResume  et le controleurPartieReseau .

--- app/src/main/java/ca/cours5b5/philippechevry/global/GConstantes.java
+++ app/src/main/java/ca/cours5b5/philippechevry/global/GConstantes.java
@@ -17,4 +17,6 @@ public final class GConstantes {
 
     public static final String EXTENSION_PAR_DEFAUT=".json";
     public static final String FIXME_JSON_PARTIE_RESEAU = "{\"listeCoups\":[],\"parametres\":{\"largeur\":\"7\",\"pourGagner\":\"4\",\"hauteur\":\"6\"},\"idJoueurInvite\":\"Du5ORtg1N0baUpC8nn5t7l2GucA2\",\"idJoueurHote\":\"bxIfyak79LMxLgNmhd985sf2izr1\"}";
+    public static final int LIMITE_MAX = 10;
+
 }
--- app/src/main/java/ca/cours5b5/philippechevry/proxy/ProxyListe.java
+++ app/src/main/java/ca/cours5b5/philippechevry/proxy/ProxyListe.java
@@ -1,14 +1,19 @@
 package ca.cours5b5.philippechevry.proxy;
 
 import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Query;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ca.cours5b5.philippechevry.controleurs.Action;
+import ca.cours5b5.philippechevry.controleurs.ControleurAction;
 import ca.cours5b5.philippechevry.controleurs.interfaces.Fournisseur;
 import ca.cours5b5.philippechevry.global.GCommande;
+import ca.cours5b5.philippechevry.global.GConstantes;
 
 public class ProxyListe  extends Proxy implements Fournisseur {
     private ChildEventListener childEventListener;
@@ -20,34 +25,73 @@ public class ProxyListe  extends Proxy implements Fournisseur {
 
     public ProxyListe(String cheminServeur) {
         super(cheminServeur);
+        noedsAjoutes = new ArrayList<>();
+
     }
+
+
 
     public void setActionNouvelItem(GCommande commande){
-
+        actionNouvelItem = ControleurAction.demanderAction(commande);
     }
     public  void ajouterValeur(Object Valeur){
-
+        DatabaseReference temporaire = super.noeudServeur.push();
+        temporaire.setValue(Valeur);
+        noedsAjoutes.add(temporaire);
     }
 
     @Override
     public void connecterAuserveur() {
         super.connecterAuserveur();
+        creerListener();
+        requete = getRequete();
+        requete.addChildEventListener(childEventListener);
     }
     private void creerListener(){
+        childEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                actionNouvelItem.setArguments(dataSnapshot.getValue().toString());
+                actionNouvelItem.executerDesQuePossible();
+            }
 
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
     }
     protected Query getRequete(){
+        return  requete = noeudServeur.orderByValue().limitToLast(GConstantes.LIMITE_MAX);
 
-        return null;
     }
 
     @Override
     public void deconnecterDuServeur() {
         super.deconnecterDuServeur();
+        requete.removeEventListener(childEventListener);
     }
 
     @Override
     public void detruireValeurs() {
+        for (DatabaseReference neaudDataReference: noedsAjoutes){
+                neaudDataReference.removeValue();
+        }
 
     }
 }
