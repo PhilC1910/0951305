b4aa44e83d828d7b5cc2fa849e70d4c756a2e6e5
philippe chevry <phil_chevry@hotmail.com>
2018-11-30 12:56:46 -0500

    j'ai creé le modele joueur il me reste la Sauvegarder le nom de l'usager courant sur le serveur,Charger le nom des joueurs du serveur et L'afficher sur la bonne vue mais j'ai un erreur avec un null pointer exception  avec MParametrePartie a la ligne 72

---
+++ app/src/main/java/ca/cours5b5/philippechevry/modeles/MJoueur.java
@@ -0,0 +1,18 @@
+package ca.cours5b5.philippechevry.modeles;
+
+import java.util.Map;
+
+import ca.cours5b5.philippechevry.exceptions.ErreurSerialisation;
+
+public class MJoueur extends Modele {
+    
+    @Override
+    public void aPartirObjetJson(Map<String, Object> objetJson) throws ErreurSerialisation {
+
+    }
+
+    @Override
+    public Map<String, Object> enObjetJson() {
+        return null;
+    }
+}
